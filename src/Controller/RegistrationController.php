ed64638f396db30a48add17ffe810210bac54747
nyasn <rnyasn@gmail.com>
2020-05-08 14:09:31 +0300

    second commit

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,154 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\Model\UserInterface;
+use FOS\UserBundle\Model\UserManagerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
+
+
+class RegistrationController extends BaseController
+{
+    private $eventDispatcher;
+    private $userManager;
+    private $tokenStorage;
+    private $tokenGenerator;
+
+
+    const ROLE_USER = 'ROLE_USER';
+
+    /**
+     * RegistrationController constructor.
+     * @param EventDispatcherInterface $eventDispatcher
+     * @param UserManagerInterface $userManager
+     * @param TokenStorageInterface $tokenStorage
+     * @param TokenGeneratorInterface $tokenGenerator
+     */
+    public function __construct(
+        EventDispatcherInterface $eventDispatcher,
+        UserManagerInterface $userManager,
+        TokenStorageInterface $tokenStorage,
+        TokenGeneratorInterface $tokenGenerator
+    )
+    {
+        $this->eventDispatcher = $eventDispatcher;
+        $this->userManager = $userManager;
+        $this->tokenStorage = $tokenStorage;
+        $this->tokenGenerator = $tokenGenerator;
+    }
+
+    /**
+     * @Route("/create-account", name="sign_up")
+     * @param Request $request
+     * @return Response
+     * @throws \Exception
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+
+        if($this->checkConnected()){
+            return $this->redirectToRoute('_login_redirect');
+        }
+        $event = new GetResponseUserEvent($user, $request);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $event = new FormEvent($form, $request);
+
+            $token = $this->tokenGenerator->generateToken();
+
+            $user->setCreatedAt(new \DateTime('now'));
+            $user->setConfirmationToken($token);
+            $user->setEnabled(false);
+            $user->setRoles([self::ROLE_USER]);
+            $this->userManager->updateUser($user);
+
+            $url = $this->generateUrl('confirmation.compte', array('token' => $token), UrlGeneratorInterface::ABSOLUTE_URL);
+
+            if (null === $response = $event->getResponse()) {
+
+                return $this->redirectToRoute('connexion');
+            }
+
+            if (null !== $response = $event->getResponse()) {
+                $this->addFlash(
+                    'notice_error',
+                    $response
+                );
+                return $this->render("fo/auth/register.html.twig",[
+                    'registrationForm' => $form->createView(),
+                    'data'=> $user
+                ]);
+            }
+        }
+        return $this->render("fo/auth/register.html.twig",[
+            'registrationForm' => $form->createView()
+        ]);
+    }
+
+
+
+    public function checkConnected(){
+        $securityContext = $this->container->get('security.authorization_checker');
+        if ($securityContext->isGranted('ROLE_USER')) {
+            return true;
+        }elseif($securityContext->isGranted('ROLE_ADMIN')){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Tell the user his account is now confirmed.
+     * @param Request $request
+     * @return Response
+     */
+    public function confirmedAction(Request $request)
+    {
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        return $this->render('@FOSUser/Registration/confirmed.html.twig', array(
+            'user' => $user,
+            'targetUrl' => $this->getTargetUrlFromSession($request->getSession()),
+        ));
+    }
+
+    /**
+     * @return string|null
+     */
+    private function getTargetUrlFromSession(SessionInterface $session)
+    {
+        $key = sprintf('_security.%s.target_path', $this->tokenStorage->getToken()->getProviderKey());
+
+        if ($session->has($key)) {
+            return $session->get($key);
+        }
+
+        return null;
+    }
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\UserBundle\Model\UserManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+
+class UserController extends AbstractController
+{
+    private $tokenManager;
+    private $session;
+    private $profileRepository;
+    private $cartService;
+    private $userManager;
+    private $entityManager;
+    private  $translator;
+
+    const ROLE_USER = 'ROLE_USER';
+
+    /**
+     * UserController constructor.
+     * @param CsrfTokenManagerInterface|null $tokenManager
+     * @param SessionInterface $session
+     * @param UserManagerInterface $userManager
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(
+        CsrfTokenManagerInterface $tokenManager = null,
+        SessionInterface $session,
+        UserManagerInterface $userManager,
+        EntityManagerInterface $entityManager
+    ) {
+        $this->tokenManager = $tokenManager;
+        $this->session = $session;
+        $this->userManager = $userManager;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param Request $request
+     *@Route("/connexion", name="connexion")
+     * @return Response
+     */
+    public function loginAction(Request $request)
+    {
+        /** @var $session Session */
+        $session = $request->getSession();
+
+        $authErrorKey = Security::AUTHENTICATION_ERROR;
+        $lastUsernameKey = Security::LAST_USERNAME;
+
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has($authErrorKey)) {
+            $error = $request->attributes->get($authErrorKey);
+        } elseif (null !== $session && $session->has($authErrorKey)) {
+            $error = $session->get($authErrorKey);
+            $session->remove($authErrorKey);
+        } else {
+            $error = null;
+        }
+
+        if (!$error instanceof AuthenticationException) {
+            $error = null; // The value does not come from the security component.
+        }
+
+        if ($this->checkConnected()) {
+            return $this->redirectToRoute('_login_redirect');
+        }
+
+        // last username entered by the user
+        $lastUsername =
+            null === $session ? '' : $session->get($lastUsernameKey);
+
+        $csrfToken = $this->tokenManager
+            ? $this->tokenManager->getToken('authenticate')->getValue()
+            : null;
+
+        return $this->renderLogin(array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'csrf_token' => $csrfToken
+        ));
+    }
+
+    /**
+     * @param array $data
+     * @return Response
+     */
+    public function renderLogin(array $data)
+    {
+        return $this->render('fo/auth/login.html.twig', $data);
+    }
+
+    public function checkConnected()
+    {
+        $securityContext = $this->container->get(
+            'security.authorization_checker'
+        );
+        if ($securityContext->isGranted('ROLE_USER')) {
+            return true;
+        } elseif ($securityContext->isGranted('ROLE_ADMIN')) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @Route("/connexion/redirect", name="_login_redirect")
+     */
+    public function redirectAction()
+    {
+        $authChecker = $this->container->get('security.authorization_checker');
+
+
+        //////////////////////////////////////////////
+
+
+        /////////////////////////////////////////////
+
+        $cookie = new Cookie(
+            'connected',
+            true,
+            time() + 3600 * 24 * 60,
+            '/',
+            null,
+            false,
+            false
+        );
+
+        if ($authChecker->isGranted('ROLE_ADMIN')) {
+            $response = $this->redirectToRoute('home_admin');
+            $response->headers->setCookie($cookie);
+
+            return $response;
+        } elseif ($authChecker->isGranted('ROLE_USER')) {
+            $response = $this->redirectToRoute('home');
+            $response->headers->setCookie($cookie);
+
+            return $response;
+        }
+        $response = $this->redirectToRoute('home');
+        $response->headers->setCookie($cookie);
+
+        return $response;
+    }
+
+
+}
